dc12907280d410e2b3223d8d63f326f242e5f48a
Mojtaba Peyrovi <mojtaba.peyrovi@gmail.com>
2023-08-20 09:21:37 +0200

    removed cards with scientific notation as card number

--- models/staging/transaction/stg_transaction__transaction.sql
+++ models/staging/transaction/stg_transaction__transaction.sql
@@ -7,11 +7,13 @@ with source as (
         category_name as product_category_name, 
         amount, 
         status, 
-        REPLACE(card_number, ' ', '')card_number_2, 
+        REPLACE(card_number, ' ', '')card_number, 
         cvv,
         format_date("%Y-%m-%d %H:%M:%S", PARSE_TIMESTAMP('%m/%d/%Y %H:%M:%S', created_at)) as created_at,
         format_date("%Y-%m-%d %H:%M:%S", PARSE_TIMESTAMP('%m/%d/%Y %H:%M:%S', happened_at)) as happened_at
-    from {{ source('transaction','transaction') }}    
+    from {{ source('transaction','transaction') }}   
+    
 )
 
 select * from source
+where source.card_number not like '%+%' 
